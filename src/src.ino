6cc20524eff01e3807d706ce5d2ff04908043188
thiagolms <thiagopinheiiro@live.com>
2023-09-29 19:37:59 -0300

    display e sdcard

--- src/src.ino
+++ src/src.ino
@@ -1,23 +1,22 @@
 #include <ArduinoJson.h>
 #include <SD.h>
-
+#include <Wire.h>;
+#include <LiquidCrystal_I2C.h>;
 
 const float sensorPin = A0;
-const int redLedPin = 5;
-const int greenLedPin = 6;
-const int yellowLedPin = 7;
 const int chipSelect = 10;
-
+LiquidCrystal_I2C lcd(0x27,16,2);
 File dados;
 
 void setup()
 {
-  pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
-  pinMode(yellowLedPin, OUTPUT);
+  lcd.init();
+  lcd.backlight();
   Serial.begin(9600);
   Serial.print("Sensor aquecendo\n");
-  delay(10000);
+  lcd.setCursor(0, 0); //coluna 0 linha 0
+  lcd.print("Aquecendo...");
+  delay(2000);
 
   if (!SD.begin(chipSelect))
   {
@@ -62,35 +61,32 @@ void loop()
   float sensorValue = analogRead(sensorPin);
   // Serial.print("Valor do sensor: ");
   // Serial.println(sensorValue);
-
-  if (sensorValue >= 400)
+  lcd.setCursor(0, 0); //coluna 0 linha 0
+  lcd.print("Medindo...");
+  
+  if (sensorValue >= 600)
   {
-    digitalWrite(greenLedPin, LOW);
-    digitalWrite(redLedPin, HIGH);
-    digitalWrite(yellowLedPin, LOW);
+	lcd.setCursor(0, 0); //coluna 0 linha 0
+	lcd.print("Ta preso!");
   }
-  else if (sensorValue >= 200)
+  else if (sensorValue >= 400)
   {
-    digitalWrite(greenLedPin, LOW);
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(yellowLedPin, HIGH);
+  lcd.setCursor(0, 0); //coluna 0 linha 0
+	lcd.print("Bebeu demais!!");
   }
-  else if (sensorValue > 150 && sensorValue <= 195)
+  else if (sensorValue > 300 && sensorValue <= 350)
   {
-    digitalWrite(greenLedPin, HIGH);
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(yellowLedPin, LOW);
+  lcd.setCursor(0, 0); //coluna 0 linha 0
+	lcd.print("Tomou umas");
   }
   else
   {
-    digitalWrite(greenLedPin, LOW);
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(yellowLedPin, LOW);
+	lcd.setCursor(0, 0); //coluna 0 linha 0
+	lcd.print("Nada de alcool");
   }
 
   salvarDados(sensorValue);
 
   delay(10000);
-
   lerDados();
 }
